ebfa4b78aa7b2c37ab6f4508dc954ae44ed04c5b
Armando Gonzalez <armando.gonzalez@correo.unimet.edu.ve>
2024-03-17 22:25:44 -0600

    stream

--- main.dart
+++ main.dart
@@ -1,31 +1,6 @@
-
 void main() async {
-  print('Inicio del programa');
-
-  try{
-    final value = await httpGet('https//fernando-herrera.com/cursos');
-    print('Éxito: $value');
-  } on Exception catch(err){
-    print('Tenemos una Exception $err');
-  }
-  catch (error){
-    print('Ups algo terrible pasó ${error}');
-  }
-  finally{
-    print('Fin del try-catch');
-  }
-
-  print('Fin del programa');
 
 
-}
 
-Future<String> httpGet(String url) async {
-  await Future.delayed(const Duration(seconds: 1));
 
-  // throw Exception('No hay parámetros en el URL');
-
-  throw 'Error en la petición';
-
-  // return 'Tenemos un valor de la petición';
 }
